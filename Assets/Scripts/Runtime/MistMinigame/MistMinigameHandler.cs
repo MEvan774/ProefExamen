6accc70b350f0ca6ee1ff51633fc50fef22eb52c
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2023-02-23 14:59:49 +0100

    Added mist minigame
    23/02/2023

---
+++ Assets/Scripts/Runtime/MistMinigame/MistMinigameHandler.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MistMinigameHandler : MonoBehaviour
+{
+    [SerializeField] private AudioPitch_Player1 _audioInput;
+
+    private MeshRenderer _mistRenderer;
+
+    private float _pitchInput;
+    private float _mistAmount = 1;
+
+    private bool _isGameClear = false;
+    private bool _isLookingAt = false;
+
+    void Start()
+    {
+        _mistRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        _pitchInput = _audioInput.CurrentPublicPitch;
+
+        if (_pitchInput > 5)
+        {
+            _mistAmount -= (Time.deltaTime / 10);
+            _mistRenderer.material.SetFloat("_Alpha", _mistAmount);
+
+            Debug.Log("mistAmount = " + _mistAmount);
+        }
+        if (_mistAmount < 0.05f && _isLookingAt && !_isGameClear)
+        {
+            _isGameClear = true;
+            OnMinigameClear();
+        }
+    }
+
+    void OnMinigameClear()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isLookingAt = true;
+        Debug.Log("ISLOOKING AT!!!!!!!!!!!");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isLookingAt = false;
+        Debug.Log("IS !NOT! LOOKING AT!!!!!!!!!!!");
+    }
+}
